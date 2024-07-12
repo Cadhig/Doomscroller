7941f6019aae3cf16bc861f1f02a585ddfc64683
Cadence K <cadencehiggins@gmail.com>
2024-07-11 21:58:18 -0500

    Created, styled feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 import Header from './components/Header'
 import StoryCarousel from './components/StoryCarousel'
-
+import Feed from './components/Feed'
 function App() {
 
   return (
     <div className='h-svh'>
       <Header />
       <StoryCarousel />
+      <Feed />
     </div>
   )
 }
---
+++ src/components/Feed.tsx
@@ -0,0 +1,36 @@
+import { feed, Post } from "../data"
+import { Heart, MessageCircle, Forward } from "lucide-react"
+export default function Feed() {
+
+    return (
+        <div>
+            {feed.map((feedData: Post, index: number) => {
+                return <div key={index} className="flex flex-col gap-2">
+                    <div className="flex gap-2 items-center mx-2">
+                        <img src={feedData.profileImg} alt={feedData.username} className="w-8 h-8 rounded-full object-cover" />
+                        <div className="text-sm">
+                            <p className="font-bold">{feedData.username}</p>
+                            <p className="text-black/70 text-xs">{feedData.location}</p>
+                        </div>
+                    </div>
+                    <img src={feedData.image} alt="post image" />
+                    <div className="flex gap-4 mx-2">
+                        <Heart />
+                        <MessageCircle />
+                        <Forward />
+                    </div>
+                    <div className="mx-2 mb-4">
+                        <p className="font-bold text-sm">{feedData.likes} likes</p>
+                        <div className="flex">
+                            <p className="font-bold">{feedData.username}</p>
+                            {feedData.description}
+                        </div>
+                        <p className="text-xs text-black/50">{feedData.time}</p>
+                    </div>
+                </div>
+            })
+            }
+        </div>
+
+    )
+}
