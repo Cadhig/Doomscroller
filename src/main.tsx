2326c2ccf6be7c76f4a2d4fd48c8a4bc5cdc36bb
Cadence K <cadencehiggins@gmail.com>
2024-07-14 22:12:38 -0500

    fixed deployment issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './routes/App.tsx'
 import Profile from './routes/Profile.tsx'
 import './index.css'
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
 } from "react-router-dom";
 
 const router = createBrowserRouter([
