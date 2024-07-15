1d96e80270041b355856eb0ee482e04fbaae6d78
Cadence K <cadencehiggins@gmail.com>
2024-07-14 21:43:19 -0500

    added hover states for desktop

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -5,30 +5,30 @@ export default function ActionBar() {
     return (
         <div className="flex items-center justify-around h-14 fixed bottom-0 lg:static lg:w-1/4 lg:flex-col lg:h-full lg:justify-normal lg:items-start lg:gap-16 bg-white w-full lg:p-4 ">
             <DesktopHeader />
-            <div className="flex items-center gap-4 text-lg font-bold">
+            <div className="flex items-center gap-4 text-lg font-bold cursor-pointer">
                 <House size={28} />
-                <p className="hidden lg:inline">Home</p>
+                <p className="hidden lg:inline hover:underline">Home</p>
             </div>
-            <div className="flex items-center gap-4 text-lg font-bold">
+            <div className="flex items-center gap-4 text-lg font-bold cursor-pointer">
                 <Heart size={28} className="hidden lg:inline" />
-                <p className="hidden lg:inline">Notifications</p>
+                <p className="hidden lg:inline hover:underline ">Notifications</p>
             </div>
-            <div className="flex items-center gap-4 text-lg font-bold">
+            <div className="flex items-center gap-4 text-lg font-bold cursor-pointer">
                 <Search size={28} />
-                <p className="hidden lg:inline">Search</p>
+                <p className="hidden lg:inline hover:underline">Search</p>
             </div>
-            <div className="flex items-center gap-4 text-lg font-bold">
+            <div className="flex items-center gap-4 text-lg font-bold cursor-pointer">
                 <SquarePlus size={28} />
-                <p className="hidden lg:inline">Create</p>
+                <p className="hidden lg:inline hover:underline">Create</p>
             </div>
-            <div className="flex items-center gap-4 text-lg font-bold">
+            <div className="flex items-center gap-4 text-lg font-bold cursor-pointer">
                 <MessageCircleMore size={28} className="hidden lg:inline" />
-                <p className="hidden lg:inline">Messages</p>
+                <p className="hidden lg:inline hover:underline">Messages</p>
             </div>
             <Clapperboard size={28} className="inline lg:hidden" />
-            <div className="flex items-center gap-4 text-lg font-bold">
+            <div className="flex items-center gap-4 text-lg font-bold cursor-pointer">
                 <CircleUser size={28} />
-                <p className="hidden lg:inline">Profile</p>
+                <p className="hidden lg:inline hover:underline">Profile</p>
             </div>
         </div>
     )
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -7,22 +7,22 @@ export default function Feed() {
             {feed.map((feedData: Post, index: number) => {
                 return <div key={index} className="flex flex-col gap-2">
                     <div className="flex gap-2 items-center mx-2">
-                        <img src={feedData.profileImg} alt={feedData.username} className="w-8 h-8 rounded-full object-cover" />
+                        <img src={feedData.profileImg} alt={feedData.username} className="w-8 h-8 rounded-full object-cover cursor-pointer" />
                         <div className="text-sm w-full">
-                            <p className="font-bold">{feedData.username}</p>
-                            <p className="text-black/70 text-xs">{feedData.location}</p>
+                            <p className="font-bold cursor-pointer hover:text-black/80">{feedData.username}</p>
+                            <p className="text-black/70 hover:underline cursor-pointer text-xs">{feedData.location}</p>
                         </div>
                     </div>
                     <img src={feedData.image} alt="post image" />
-                    <div className="flex gap-4 mx-2">
+                    <div className="flex gap-4 mx-2 cursor-pointer">
                         <Heart />
                         <MessageCircle />
                         <Forward />
                     </div>
                     <div className="mx-2 mb-4 ">
-                        <p className="font-bold text-sm">{feedData.likes} likes</p>
+                        <p className="font-bold text-sm cursor-pointer hover:underline">{feedData.likes} likes</p>
                         <div className="inline">
-                            <p className="font-bold w-20 inline">{feedData.username}</p>
+                            <p className="font-bold w-20 inline hover:text-black/80 cursor-pointer">{feedData.username}</p>
                             <p className="ml-1 inline">{feedData.description}</p>
                         </div>
                         <p className="text-xs text-black/50">{feedData.time}</p>
