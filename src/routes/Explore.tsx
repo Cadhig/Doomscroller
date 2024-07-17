1288c440032899e106281a837ba88b19cddedad4
Cadence K <cadencehiggins@gmail.com>
2024-07-16 21:27:27 -0500

    desktop view for explore

--- src/routes/Explore.tsx
+++ src/routes/Explore.tsx
@@ -2,21 +2,24 @@ import Header from "../components/MobileHeader"
 import DesktopHeader from "../components/DesktopHeader"
 import ActionBar from "../components/ActionBar"
 import { explore, Images } from "../data"
+import FollowSuggestions from "../components/FollowSuggestion"
 export default function Explore() {
 
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col lg:flex-row-reverse lg:justify-between h-svh hideScrollbar">
+            <FollowSuggestions />
             <Header />
-            <DesktopHeader />
-            <div className="w-full flex items-center justify-center">
-                <input type="text" placeholder="explore" className="bg-black/10 w-3/4 border rounded-xl px-1" />
-            </div>
-            <div className="grid grid-cols-3 w-full gap-1">
-                {explore.map((exploreData: Images, index: number) => {
-                    return <div key={index}>
-                        <img src={exploreData.img} alt="exploreImg" className="w-full h-32 object-cover" />
-                    </div>
-                })}
+            <div className=" flex flex-col gap-4 m-4 lg:w-1/2 overflow-auto hideScrollbar">
+                <div className="flex justify-center">
+                    <input type="text" placeholder="explore" className="bg-black/10 w-3/4 border rounded-xl px-1" />
+                </div>
+                <div className="grid grid-cols-3 w-full gap-1">
+                    {explore.map((exploreData: Images, index: number) => {
+                        return <div key={index}>
+                            <img src={exploreData.img} alt="exploreImg" className="w-full h-full object-cover" />
+                        </div>
+                    })}
+                </div>
             </div>
             <ActionBar />
         </div>
