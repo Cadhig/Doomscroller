bd89a2f4f4ff2f3777331b4b06fce177f038e76c
Cadence K <cadencehiggins@gmail.com>
2024-07-16 19:02:51 -0500

    adjusted message view for desktop

--- src/routes/Dms.tsx
+++ src/routes/Dms.tsx
@@ -2,6 +2,7 @@ import ActionBar from "../components/ActionBar"
 import { SquarePen } from "lucide-react"
 import { Messages, messages, messageRequests } from "../data"
 import { useState } from "react"
+import FollowSuggestions from "../components/FollowSuggestion"
 export default function Dms() {
     const [isPrimary, setIsPrimary] = useState(true)
     const messageView = isPrimary ? messages : messageRequests
@@ -9,18 +10,19 @@ export default function Dms() {
     const requestsClasses = isPrimary ? 'w-1/2 border rounded bg-black/50 text-white' : 'w-1/2 border rounded bg-[#4c68d7] text-white'
 
     return (
-        <div>
-            <div className="flex flex-col gap-4 m-4">
+        <div className="lg:flex lg:flex-row-reverse lg:justify-between h-svh hideScrollbar w-full">
+            <FollowSuggestions />
+            <div className="flex flex-col gap-4 m-4 lg:w-1/2">
                 <div className="flex justify-between items-center">
                     <h1 className="text-lg font-bold">Messages</h1>
                     <SquarePen />
                 </div>
-                <div className="flex gap-2">
+                <div className="flex gap-2 w-full">
                     <button onClick={() => setIsPrimary(true)} className={primaryClasses}>Primary</button>
                     <button onClick={() => setIsPrimary(false)} className={requestsClasses}>Requests</button>
                 </div>
                 {messageView.map((messageData: Messages, index: number) => {
-                    return <div key={index} className="flex gap-2">
+                    return <div key={index} className="flex gap-2 w-full">
                         <img src={messageData.img} alt={messageData.username} className="w-12 h-12 object-cover border rounded-full" />
                         <div className="flex flex-col w-1/2">
                             <p>{messageData.username}</p>
